55e540a25d9fa80ee3c9b08c5c4bbc13d0cdb969
Arup <arup03.paul@gmail.com>
2025-09-11 17:59:03 +0530

    Changing model

--- app.py
+++ app.py
@@ -77,7 +77,7 @@ def get_conversation_chain(vectorstore):
         return None
 
     llm = ChatGroq(
-        model_name="llama3-8b-8192"
+        model_name="llama-3.1-8b-instant"
     )
 
     # Define system message
